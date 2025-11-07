d6019ff60abd70a76f845e0012f4296800280ce1
wangmingming <1960588251@qq.com>
2025-11-07 14:13:09 +0800

    Update main.go to include current timestamp in response message

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World! %s", time.Now().Format("2006-01-02 15:04:05"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
